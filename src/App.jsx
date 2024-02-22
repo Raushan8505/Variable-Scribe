616dcd84e4af28bf58ab445f3d7d3835344ef437
MayankBelwal05 <mbelwal05@gmail.com>
2024-02-22 23:31:18 +0530

    day2changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 
 import './App.css'
 import Navbartrio from './components/Navbartrio'
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import Teamcard from './components/Teamcards'
+
 
 function App() {
- 
+
 
   return (
     <>
-     <Navbartrio/>
+      <Navbartrio />
+      <Teamcard />
     </>
   )
 }
---
+++ src/components/Teamcards.jsx
@@ -0,0 +1,84 @@
+import Card from 'react-bootstrap/Card';
+
+function Teamcard() {
+    return (
+        <div className="team">
+{/* <h1>Our Team Members</h1> */}
+            <Card style={{ width: '18rem'}}>
+                <Card.Img variant="top" src="holder.js/100px180"  />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the cards content.
+                    </Card.Text>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Card Link</Card.Link>
+                </Card.Body>
+            </Card>
+
+            <Card style={{ width: '18rem'}}>
+                <Card.Img variant="top" src="holder.js/100px180"  />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the cards content.
+                    </Card.Text>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Card Link</Card.Link>
+                </Card.Body>
+            </Card>
+
+            <Card style={{ width: '18rem'}}>
+                <Card.Img variant="top" src="holder.js/100px180"  />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the cards content.
+                    </Card.Text>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Card Link</Card.Link>
+                </Card.Body>
+            </Card>
+
+            <Card style={{ width: '18rem'}}>
+                <Card.Img variant="top" src="holder.js/100px180"  />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the cards content.
+                    </Card.Text>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Card Link</Card.Link>
+                </Card.Body>
+            </Card>
+
+            <Card style={{ width: '18rem'}}>
+                <Card.Img variant="top" src="holder.js/100px180"  />
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                    <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the cards content.
+                    </Card.Text>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Card Link</Card.Link>
+                </Card.Body>
+            </Card>
+
+            
+
+          
+        </div>
+    );
+}
+
+export default Teamcard;
